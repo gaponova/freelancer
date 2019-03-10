81d3f756d85168ce8f669a7a954a5373ec33e6c4
gaponova <dzensmart@gmail.com>
2019-03-10 18:53:35 +0200

    Добавили тест для скрытия мобильного меню при клике

--- js/adaptive-menu.js
+++ js/adaptive-menu.js
@@ -1,13 +1,15 @@
 $(function() {
     
-   $('.menuToggle').on('click', function(){
-			$('.main-nav').slideToggle(500, function(){
-				if($(this).css('display') === 'none'){
-					$(this).removeAttr('style');
-				}
-			});
-		});
+	$('.menuToggle').on('click', function(){
+		 $('.main-nav').slideToggle(500, function(){
+			 if($(this).css('display') === 'none'){
+				 $(this).removeAttr('style');
+			 }
+		 });
+		 $('.main-nav__link').on('click', function(){
+			 $('.main-nav').hide();
+	 });
+	 });
 });
-
 
 
